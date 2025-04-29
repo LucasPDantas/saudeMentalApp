34e102fde4e6befe2fe55d05b8c8baf5418dbf3e
GabrielDutraLima <gabriel-dutralima@outlook.com>
2025-04-28 22:38:18 -0300

    feat: Integrado projeto existente ao repositório

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,8 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.runtime.livedata)
     testImplementation(libs.junit)
+    implementation("androidx.compose.material:material-icons-extended")
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
--- app/src/main/java/com/example/safelife/model/Usuario.kt
+++
@@ -1,7 +0,0 @@
-package com.example.safelife.model
-
-data class Usuario(
-    val uid: String = "",
-    val name: String? = null,
-    val email: String? = null
-)
